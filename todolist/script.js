611f1f70823a523eee3547bce213915c64b41def
JamilaJoya <32989684+JamilaJoya@users.noreply.github.com>
2023-02-28 00:09:05 +0200

    Update script.js
    more functionality added

--- todolist/script.js
+++ todolist/script.js
@@ -2,12 +2,30 @@ var button = document.getElementById('enter');
 var input = document.getElementById('userinput');
 var ul = document.querySelector('ul');
 
-button.addEventListener('click', function() {
+ function inputLength(){
+    return input.value.length;
+}
+
+function createListElement(){
     var li = document.createElement('li');
-    li.appendChild(document.createTextNode(input.value));
-    ul.appendChild(li);
-    input.value = '';
+        li.appendChild(document.createTextNode(input.value));
+        ul.appendChild(li);
+        input.value = '';
+}
 
-});
+function addListAfterClick(){
+    if(inputLength() > 0) {
+        createListElement();
+    }
+}
 
-/* practice: make a todo lsit. how to add todo */ 
+function addListAfterKeypress(event){
+    if(inputLength() > 0 && event.keyCode === 13) {
+        createListElement();
+    }
+}
+
+button.addEventListener('click', addListAfterClick);
+input.addEventListener('keypress', addListAfterKeypress);
+
+
